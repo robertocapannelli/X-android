879257d088012698743ab50877a3e7fae16f3949
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-06-29 11:14:14 +0200

    university activity

---
+++ app/src/main/java/com/walkap/x_android/UniversityActivity.java
@@ -0,0 +1,24 @@
+package com.walkap.x_android;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class UniversityActivity extends AppCompatActivity {
+
+    private FragmentPagerAdapter mPagerAdapter;
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_university);
+
+        RecyclerView universities = (RecyclerView) findViewById(R.id.university_list);
+
+
+    }
+}
