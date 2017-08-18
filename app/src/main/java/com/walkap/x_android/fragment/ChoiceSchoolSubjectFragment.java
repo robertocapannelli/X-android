ecff2d054ee8d8c9de0f7537e8822ffe78121b07
luca92 <l.atturo@hotmail.it>
2017-08-18 15:51:36 +0200

    min api 21 new activity addSchedule with datapicker and days (toggleButton)

--- app/src/main/java/com/walkap/x_android/fragment/ChoiceSchoolSubjectFragment.java
+++ app/src/main/java/com/walkap/x_android/fragment/ChoiceSchoolSubjectFragment.java
@@ -65,7 +65,7 @@ public class ChoiceSchoolSubjectFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choice_school_subject, container, false);
+        return inflater.inflate(R.layout.add_schedule, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
