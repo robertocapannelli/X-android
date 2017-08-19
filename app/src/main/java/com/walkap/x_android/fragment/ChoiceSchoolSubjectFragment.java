a87877838248ca099497e0b8e2c810cbbece8e4b
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-08-19 18:57:10 +0200

    Revert "min api 21"
    This reverts commit 65fdec6b4aebb66b7a523b7a875b27e029dec410.

--- app/src/main/java/com/walkap/x_android/fragment/ChoiceSchoolSubjectFragment.java
+++ app/src/main/java/com/walkap/x_android/fragment/ChoiceSchoolSubjectFragment.java
@@ -64,7 +64,6 @@ public class ChoiceSchoolSubjectFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.add_schedule, container, false);
     }
