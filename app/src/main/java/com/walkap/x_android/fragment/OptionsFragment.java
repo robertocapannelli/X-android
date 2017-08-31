b3288f18a361be4743809c1c777cbd57438f125a
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-08-31 15:11:55 +0200

    reverted changes

--- app/src/main/java/com/walkap/x_android/fragment/OptionsFragment.java
+++ app/src/main/java/com/walkap/x_android/fragment/OptionsFragment.java
@@ -7,7 +7,6 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -115,12 +114,6 @@ public class OptionsFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onClick(View view) {
         saveData();
-
-        Fragment fragment = new HomeFragment();
-
-        FragmentManager fragmentManager = getFragmentManager();
-
-        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
     }
 
 
