0dd2da40b646cfaf600db0ddea70664cc2eccb4c
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-12-28 23:53:15 +0100

    added new method to the interface

--- app/src/main/java/com/walkap/x_android/dao/user/UserDao.java
+++ app/src/main/java/com/walkap/x_android/dao/user/UserDao.java
@@ -2,18 +2,20 @@ package com.walkap.x_android.dao.user;
 
 import com.walkap.x_android.model.user.User;
 
-import java.util.Vector;
-
 public interface UserDao {
 
-    public User getUser(int id);
+    public void getUser(String userId);
 
-    public Vector<User> getAllUsers();
+    public void getAllUsers();
 
     public void addUser(User user);
 
-    public void updateUser(User user);
+    public void updateUser(String userId, String key, Object value );
 
     public void deleteUser(User user);
+
+    public void customQuery(String key, Object value);
+
+    public void addUserIfNotPresent(User user);
 
 }
