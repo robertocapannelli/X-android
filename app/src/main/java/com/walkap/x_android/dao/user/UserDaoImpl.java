25e5a4fafcd9503dec9cf1709eaf3906937f950f
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-12-28 23:56:51 +0100

    addUserIfNotPresent() improved and fully functional, added some other method (still to test)

--- app/src/main/java/com/walkap/x_android/dao/user/UserDaoImpl.java
+++ app/src/main/java/com/walkap/x_android/dao/user/UserDaoImpl.java
@@ -1,44 +1,100 @@
 package com.walkap.x_android.dao.user;
 
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.walkap.x_android.model.user.User;
 
-import java.util.Vector;
-
-public class UserDaoImpl implements UserDao {
+public class UserDaoImpl extends AppCompatActivity implements UserDao {
 
     private static final String TAG = "UserDaoImpl";
+    private static final String COLLECTION = "users";
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference ref = db.collection(COLLECTION);
+
 
     @Override
-    public User getUser(int id) {
-
-        User user = null;
-
-        return user;
+    public void customQuery(String key, Object value){
+        Query query = ref.whereEqualTo(key, value);
+        // this could be chained with another where() e.g. whereLessThan whereEqualTo whereGreaterThanOrEqualTo
     }
 
     @Override
-    public Vector<User> getAllUsers() {
+    public void getUser(String userId) {
+        User user;
+        ref.document(userId)
+                .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                User user = documentSnapshot.toObject(User.class);
+            }
+        });
+    }
 
-        Vector<User> vector = null;
+    @Override
+    public void getAllUsers() {
 
-        return vector;
+        ref.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DocumentSnapshot document : task.getResult()) {
+                        Log.d(TAG, document.getId() + " => " + document.getData());
+                    }
+                } else {
+                    Log.w(TAG, "Error getting documents.", task.getException());
+                }
+            }
+        });
+    }
 
+
+    @Override
+    public void addUserIfNotPresent(User user){
+        final User checkUser = user;
+        Log.d(TAG, "addUserIfNotPresent(): " + user.getEmail());
+        ref.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    Boolean found = false;
+                    for (DocumentSnapshot document : task.getResult()) {
+                        User user = document.toObject(User.class);
+                        Log.d(TAG, "addUserIfNotPresent(): " + user.getEmail());
+                        if(user.getEmail().equals(checkUser.getEmail())){
+                            found = true;
+                            break;
+                        }
+                        Log.d(TAG, document.getId() + " => " + document.getData());
+                    }
+                    if(!found){
+                        Log.d(TAG, "addUserIfNotPresent(): The user email doesn't match, so we can add new user!" );
+                        addUser(checkUser);
+                    }
+
+                } else {
+                    Log.w(TAG, "Error getting documents.", task.getException());
+                }
+            }
+        });
     }
 
     @Override
     public void addUser(User user) {
-
-        db.collection("users")
-                .add(user)
+        // Add a new document with a generated ID
+        ref.add(user) //add generate a unique id for the document id
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
@@ -51,23 +107,45 @@ public class UserDaoImpl implements UserDao {
                         Log.w(TAG, "Error adding document", e);
                     }
                 });
-
     }
 
     @Override
-    public void updateUser(User user) {
+    public void updateUser(String userId, String key, Object value) {
+
+        ref.document(userId)
+                .update(key, value)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully updated!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error updating document", e);
+                    }
+                });
 
     }
 
     @Override
     public void deleteUser(User user) {
 
+        ref.document(user.getUserId())
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error deleting document", e);
+                    }
+                });
+
     }
-
-    public static void main(String[] args) {
-
-        User user = new User();
-
-    }
-
 }
