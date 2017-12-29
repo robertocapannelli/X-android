cbcb07ffa39ab67479469ebaa89e5d1a8d0254fb
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-12-29 10:19:47 +0100

    added comments and reformat code

--- app/src/main/java/com/walkap/x_android/activity/ForgotPasswordActivity.java
+++ app/src/main/java/com/walkap/x_android/activity/ForgotPasswordActivity.java
@@ -29,16 +29,18 @@ public class ForgotPasswordActivity extends BaseActivity implements View.OnClick
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
-
         //Get the editText
         mYourEmail = (EditText) findViewById(R.id.your_email);
         //Get button reset password
         mResetPassword = (TextView) findViewById(R.id.reset_password_button);
-
         mResetPassword.setOnClickListener(this);
-
     }
 
+    /**
+     * This method is used to validate the field form
+     *
+     * @return - boolean
+     */
     private boolean validateForm() {
         boolean result = true;
         if (TextUtils.isEmpty(mYourEmail.getText().toString())) {
@@ -50,8 +52,10 @@ public class ForgotPasswordActivity extends BaseActivity implements View.OnClick
         return result;
     }
 
+    /**
+     * This method reset the password just in case the form validate
+     */
     public void resetPassword() {
-
         if (!validateForm()) {
             return;
         }
@@ -59,16 +63,13 @@ public class ForgotPasswordActivity extends BaseActivity implements View.OnClick
         auth = FirebaseAuth.getInstance();
         //Get string from edit text
         emailAddress = mYourEmail.getText().toString();
-
         auth.sendPasswordResetEmail(emailAddress).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-
                 if (task.isSuccessful()) {
                     Log.d(TAG, "Email sent.");
                     Toast.makeText(ForgotPasswordActivity.this, R.string.email_sent, Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(ForgotPasswordActivity.this, MainActivity.class));
-
                 } else {
                     Log.d(TAG, "Email error.");
                     Toast.makeText(ForgotPasswordActivity.this, R.string.email_not_sent, Toast.LENGTH_SHORT).show();
@@ -77,9 +78,13 @@ public class ForgotPasswordActivity extends BaseActivity implements View.OnClick
         });
     }
 
+    /**
+     * This method is ha listener for the form button
+     *
+     * @param view - View
+     */
     @Override
     public void onClick(View view) {
-
         resetPassword();
     }
 }
