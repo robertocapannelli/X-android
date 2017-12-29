046d82e337a135446b6f63f7bb031ff3c048e512
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-12-29 12:37:28 +0100

    added comments and changed the method's name

--- app/src/main/java/com/walkap/x_android/activity/BaseActivity.java
+++ app/src/main/java/com/walkap/x_android/activity/BaseActivity.java
@@ -6,7 +6,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,46 +14,58 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class BaseActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
-
     private static final String TAG = "BaseActivity";
-
     //Firebase instance variables
-    public FirebaseAuth mFirebaseAuth;
-    public FirebaseUser mFirebaseUser;
-    public DatabaseReference mDatabase;
-
+    protected FirebaseAuth mFirebaseAuth;
+    protected FirebaseUser mFirebaseUser;
+    protected DatabaseReference mDatabase;
     private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
-
     }
 
+    /**
+     * This method shows the progress dialog
+     * during the loading
+     */
     public void showProgressDialog() {
         if (mProgressDialog == null) {
             mProgressDialog = new ProgressDialog(this);
             mProgressDialog.setCancelable(false);
             mProgressDialog.setMessage("Loading...");
         }
-
         mProgressDialog.show();
     }
 
+    /**
+     * This method hide the progress dialog
+     * once the process has finished
+     */
     public void hideProgressDialog() {
         if (mProgressDialog != null && mProgressDialog.isShowing()) {
             mProgressDialog.dismiss();
         }
     }
 
-    public String getUid() {
-        return FirebaseAuth.getInstance().getCurrentUser().getUid();
+    /**
+     * This method is used to retrieve the Firebase user id
+     * from the current instance
+     * @return - String
+     */
+    public String getFirebaseUserId() {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        return firebaseUser.getUid();
     }
 
+    /**
+     * This method retrieve a log when connection failed
+     * @param connectionResult - ConnectionResult
+     */
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         // An unresolvable error has occurred and Google APIs (including Sign-In) will not
@@ -62,5 +73,4 @@ public class BaseActivity extends AppCompatActivity implements GoogleApiClient.O
         Log.d(TAG, "onConnectionFailed:" + connectionResult);
         Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
     }
-
 }
